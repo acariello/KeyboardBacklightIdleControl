822be104a2d62896125c735f18f50b8572867efd
acariello <39349280+acariello@users.noreply.github.com>
2020-02-05 16:48:23 -0500

    Add files via upload

---
+++ BackLightProg/BackLightProg/LASTINPUTINFO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BackLightProg
+{
+      [StructLayout(LayoutKind.Sequential)]
+      public struct LASTINPUTINFO 
+   {
+         public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+         [MarshalAs(UnmanagedType.U4)]
+         public UInt32 cbSize;
+         [MarshalAs(UnmanagedType.U4)]
+         public UInt32 dwTime;
+
+
+   }
+   
+}
---
+++ BackLightProg/BackLightProg/MethodClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BackLightProg
+{
+   static class MethodClass
+   {
+
+      public static MethodInfo GetRuntimeMethodsExt(this Type type, string name, params Type[] types)
+      {
+         // Find potential methods with the correct name and the right number of parameters and parameter names
+         var potentials = (from ele in type.GetMethods()
+                           where ele.Name.Equals(name)
+                           select ele);
+
+         // check if we have more than 1? Or not?
+         return potentials.FirstOrDefault();
+      }
+
+
+   }
+}
